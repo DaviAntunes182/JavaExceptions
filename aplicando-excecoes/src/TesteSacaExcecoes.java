9df87c97d20d7d3a89389d6dff1402b2d6d250ba
DaviAntunes182 <davi.antunes@bb.com.br>
2024-10-10 09:22:20 -0300

    Aplicando exceções nas classes de conta do curso anterior e entendendo diferenças de checked e unchecked na prática

---
+++ aplicando-excecoes/src/TesteSacaExcecoes.java
@@ -0,0 +1,19 @@
+public class TesteSacaExcecoes {
+    public static void main(String[] args) {
+        Conta conta = new ContaCorrente(123, 321);
+
+        conta.depositar("200");
+        try {// O try é usado na classe main por que o método chamado pode gerar um erro
+            conta.sacar("210");
+        }catch (SaldoInsucienteException ex){
+            System.out.println(ex.getMessage());
+        }
+        try{
+            conta.sacar("-1");
+        }catch (SaldoInsucienteException | ValorNegativoException ex){
+            System.out.println(ex.getMessage());
+        }
+
+        System.out.println(conta.getSaldo());
+    }
+}
